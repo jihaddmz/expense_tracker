a7cf9c300b3d04e3b3966d0d9d9c7299045fa978
jihaddmz <jihadmahfouz0@gmail.com>
2025-02-13 15:39:15 +0200

    - removed the unused HelpersharedPref file
    - Removed the unused methods inside the helper file

--- lib/core/helpers/helper.dart
+++ lib/core/helpers/helper.dart
@@ -1,9 +1,4 @@
-import 'dart:math';
 
 class Helpers {
-  static String generateRandomString(int length) {
-    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    Random rnd = Random();
-    return List.generate(length, (index) => chars[rnd.nextInt(chars.length)]).join();
-  }
+  
 }
