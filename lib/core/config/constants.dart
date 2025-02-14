0229e247e87ce543fcfce0395a69c9ecf609e831
jihaddmz <jihadmahfouz0@gmail.com>
2025-02-14 19:35:17 +0200

    - Added sport category to constants

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -19,15 +19,16 @@ class Constants {
     'December'
   ];
   static final Map<String, Map<String, dynamic>> categories = {
-    'Shopping': {'color': colorGreen, 'icon': Icons.shopping_cart_outlined},
     'Food': {'color': colorBlue, 'icon': Icons.fastfood_outlined},
-    'Health': {'color': colorYellow, 'icon': Icons.local_hospital_outlined},
-    'Transport': {'color': colorTeal, 'icon': Icons.directions_bus_outlined},
     'Home': {'color': colorOrange, 'icon': Icons.home_outlined},
-    'Utilities': {'color': colorPink, 'icon': Icons.phone_android_outlined},
-    'Activities': {'color': colorGrey, 'icon': Icons.local_movies_outlined},
-    'Gifts': {'color': colorLightPink, 'icon': Icons.card_giftcard_outlined},
-    'Help': {'color': colorGreen, 'icon': Icons.help_outline},
+    'Health': {'color': colorYellow, 'icon': Icons.local_hospital_outlined},
     'Car': {'color': colorPurple, 'icon': Icons.directions_car_outlined},
+    'Transport': {'color': colorTeal, 'icon': Icons.directions_bus_outlined},
+    'Shopping': {'color': colorGreen, 'icon': Icons.shopping_cart_outlined},
+    'Sport': {'color': colorLightPink, 'icon': Icons.shopping_cart_outlined},
+    'Activities': {'color': colorGrey, 'icon': Icons.local_movies_outlined},
+    'Utilities': {'color': colorPink, 'icon': Icons.phone_android_outlined},
+    'Gifts': {'color': colorBlue, 'icon': Icons.card_giftcard_outlined},
+    'Help': {'color': colorOrange, 'icon': Icons.help_outline},
   };
 }
